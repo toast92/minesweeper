f094a4968b0eab06c1eb9f33d4151c9ae32e95f9
Irina Nicula <irina.nicula0@gmail.com>
2019-01-19 21:22:39 +0100

    Created setup for project and added first minesweeper grid

---
+++ src/minesweeper.js
@@ -0,0 +1,28 @@
+const blankline = '  |   |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
