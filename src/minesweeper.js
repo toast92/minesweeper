68cfa89ce7ac61673034bf2e490791fd9f35c221
Irina Nicula <irina.nicula0@gmail.com>
2019-01-19 21:35:03 +0100

    Finished first hard-coded stage of the minesweeper project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,8 +1,20 @@
-const blankline = '  |   |  ';
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
 console.log('This is what an empty board would look like:');
-console.log(blankline);
-console.log(blankline);
-console.log(blankline);
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
+
+
+
 
 
 
