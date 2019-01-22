920390fcac92bb13f5e1b8b3b187a97ae953976c
Irina Nicula <irina.nicula0@gmail.com>
2019-01-22 22:30:31 +0100

    created empty board for minesweeper game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,17 @@
+const printBoard = (board) => {
+    console.log('Current Board:');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+}
+
+const board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+];
+
+printBoard(board);
 
 
 
