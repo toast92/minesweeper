faa09b8a58730f555a7ad9a1bc2d82656e316ea0
Irina Nicula <irina.nicula0@gmail.com>
2019-01-26 00:12:08 +0100

    starting from scratch

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,4 @@
-const printBoard = (board) => {
-    console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-}
 
-const board = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
-];
-
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
 
 
 
