7346076d6f28193b21b355c3522513e7bf306a93
Irina Nicula <irina.nicula0@gmail.com>
2019-01-26 00:28:36 +0100

    created a function to generate a game board based on parameters

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,3 +1,14 @@
+const generatePlayerBoard  = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for(let i=0; i<numberOfRows; i++){
+        let row=[];
+        for(let j=0; j<numberOfColumns; j++){
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+}
 
 
 
