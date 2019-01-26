c9f69fa19e0bb4332562e5b95b26578e6effbda6
Irina Nicula <irina.nicula0@gmail.com>
2019-01-26 14:11:43 +0100

    created bomb generator function and dynamic board print function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -10,6 +10,33 @@ const generatePlayerBoard  = (numberOfRows, numberOfColumns) => {
     return board;
 }
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for(let i=0; i<numberOfRows; i++){
+        let row=[];
+        for(let j=0; j<numberOfColumns; j++){
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+       //The code in me while loop has the potential to place bombson top of already existing bombs
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * (numberOfRows-1));
+        let randomColumnIndex = Math.floor(Math.random() * (numberOfColumns-1));
+        board[randomRowIndex][randomColumnIndex] == 'B';
+        numberOfBombsPlaced++;
+    }
+    return board;
+}
+
+const printBoard = (board) => {
+    console.log(board.map(row =>
+        row.join('|')).join('/n'));
+}
+
+
+
 
 
 
